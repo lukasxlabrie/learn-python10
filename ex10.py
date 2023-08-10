9241ef01adc3ef08dbf8fb03da3c0c499a55f60d
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-09 21:45:28 -0400

    notes

--- ex10.py
+++ ex10.py
@@ -1,6 +1,8 @@
+#these are all VAR but with a variety of new functions
+#\t adds a tab
 tabby_cat = "\tI'm tabbed in."
-persian_cat = "Im split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+persian_cat = "Im split\non a line." #\n starts a new line
+backslash_cat = "I'm \\ a \\ cat." #this adds a \ between defined areas
 
 #there is no list function at work here, it's just a """" being used to allow text to travel across multiple lines
 #the * is part of the text anf the /t adds a tab AKA indent
