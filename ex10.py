0cd12c50affe01e5eb5cb102d09345d435d05177
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-09 21:42:41 -0400

    notes

--- ex10.py
+++ ex10.py
@@ -2,6 +2,8 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "Im split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
+#there is no list function at work here, it's just a """" being used to allow text to travel across multiple lines
+#the * is part of the text anf the /t adds a tab AKA indent
 fat_cat = """
 I'll do a list:
 \t* Cat food
